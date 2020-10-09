6fbd23ed1c09cf18cd6b0f53ca5bb641ef96b976
ficusrobusta <70322151+ficusrobusta@users.noreply.github.com>
2020-10-09 13:55:02 +0100

    Update medical_data_visualizer.py

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -20,20 +20,54 @@ df['gluc'] = df['gluc'].apply(lambda x: 0 if x == 1 else 1)
 # Draw Categorical Plot
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat = pd.melt(df, id_vars = ['cardio'], var_name = 'variable', value_vars = ['alco', 'active','cholesterol', 'gluc',  'overweight','smoke'])
+    df_cat = pd.melt(df, id_vars = 'cardio', var_name = 'variable', value_vars = ['alco', 'active','cholesterol', 'gluc', 'overweight','smoke'])
+
 
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the collumns for the catplot to work correctly.
-    df_cat = pd.melt(df, id_vars = ['cardio'], var_name = 'variable', value_vars = [('cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight')])
+    df_cat = pd.melt(df, var_name = 'variable', value_vars = ['active','alco','cholesterol', 'gluc','overweight','smoke'], id_vars = 'cardio')
 
     # Draw the catplot with 'sns.catplot()'
-    fig = sns.catplot(data=df_cat, kind="count",  x="variable",hue="value", col="cardio").set_axis_labels("variable", "Total")
+    fig = sns.catplot(data=df_cat, kind="count",  x="variable",hue="value", col="cardio").set_axis_labels("variable", "total")
+
+
 
     # Do not modify the next two lines
     fig.savefig('catplot.png')
     return fig
 
+
 # Draw Heat Map
 def draw_heat_map():
+    # Clean the data
+    df_heat = df[(df['ap_lo']<=df['ap_hi']) &
+    (df['height'] >= df['height'].quantile(0.025))&
+    (df['height'] <= df['height'].quantile(0.975))&
+    (df['weight'] >= df['weight'].quantile(0.025))&
+    (df['weight'] <= df['weight'].quantile(0.975))
+    ]
+
+    # Calculate the correlation matrix
+    corr = df_heat.corr().round(1)
+    
+
+    # Generate a mask for the upper triangle
+    mask = np.triu(corr)
+    
+    
+
+    # Set up the matplotlib figure
+    fig, ax = plt.subplots(figsize=(9,9))
+
+    # Draw the heatmap with 'sns.heatmap()'
+    mask = np.zeros_like(corr)
+    
+    heat = sns.heatmap(corr,mask=mask, fmt='.1f',vmax=.8, linewidths=.5,square=True, cbar_kws = {'shrink':0.5},annot=True, center=0)
+
+
+    # Do not modify the next two lines
+    fig.savefig('heatmap.png')
+    return fig
+
     # Clean the data
     df_heat = df[(df['ap_lo']<=df['ap_hi']) &
     (df['height'] >= df['height'].quantile(0.025))&
